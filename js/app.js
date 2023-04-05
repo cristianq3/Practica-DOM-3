d4c241146708b040bf101246ad83d797b6d92edf
cristianq3 <cristianq3@gmail.com>
2023-04-04 22:09:30 -0300

    Completado el maquetado de Ejercicio3

--- js/app.js
+++ js/app.js
@@ -1,3 +1,1 @@
-/* 1- Crea una web con bootstrap y js, que contenga un botón comenzar el juego, en ese momento se crea un número aleatorio que el usuario deberá adivinar, la interfaz del usuario debe tener además un input para ingresar un número y un botón enviar, al presionar el botón enviar mostrar en un alert si el usuario adivino o no el número mágico, si no lo adivino indicarle con un alert si el numero que ingreso es mayor o menor al número mágico.
-Cuando el usuario adivine el numero mostrar un mensaje indicando al usuario que adivino el numero. */
-
+// 3 - Crea una web con bootstrap y js, que contenga un botón input donde se pueda cargar una tarea y un botón que al ser presionado agregue dicha tarea a una lista, cada elemento ingresado en la lista debe poder ser eliminado con un botón creado para ese fin. 
